f45e93fa867ba2ae4921e8338f7b6f637651c523
Wangwei <wangweicoder@gmail.com>
2013-12-14 00:33:13 +0800

    add myalterView animation

--- loadDemo/loadDemo/MenuViewController.m
+++ loadDemo/loadDemo/MenuViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MenuViewController.h"
+#import "MyAlterView.h"
 
 @interface MenuViewController ()
 
@@ -26,7 +27,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor orangeColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIButton *button1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button1.frame = CGRectMake(30, 50, 100, 50);
+    button1.tag = 1;
+    [button1 setTitle:@"一个按钮" forState:UIControlStateNormal];
+    [button1 addTarget:self action:@selector(showAlterView:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIButton *button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button2.frame = CGRectMake(30, 150, 100, 50);
+    button2.tag = 2;
+    [button2 setTitle:@"二个按钮" forState:UIControlStateNormal];
+    [button2 addTarget:self action:@selector(showAlterView:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button1];
+    [self.view addSubview:button2];
+}
+- (void)showAlterView:(UIButton *) sender {
+    NSString *str = nil;
+    if (sender.tag == 2) str = @"Cancel";
+    MyAlterView *alter = [[MyAlterView alloc] initWithTitle:@"My alterView" Content:@"It is my own custom alterview" sureButton:@"OK" cancelButton:str];
+    [alter show];
 }
 - (void) viewDidAppear:(BOOL)animated {
  
